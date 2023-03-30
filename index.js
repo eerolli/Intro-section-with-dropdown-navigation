5ce6363feba010af6d529ca40a61e417b8a10376
eerolli <eero_liuska_8@hotmail.com>
2023-03-30 23:08:54 +0300

    Completed project. Wrote readme

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ let subMenuFeatures = document.querySelector(".sub-menu-one");
 let subMenuCompany = document.querySelector(".sub-menu-two");
 let features = document.querySelector(".features");
 let company = document.querySelector(".company");
+let dtSubMenuFeatures = document.querySelector(".dt-sub-menu-one");
+let dtSubMenuCompany = document.querySelector(".dt-sub-menu-two");
+let dtFeatures = document.querySelector(".dt-features");
+let dtCompany = document.querySelector(".dt-company");
 
 hamburger.addEventListener('click', ()=>{
     hamburger.classList.toggle("active");
@@ -20,4 +24,14 @@ features.addEventListener('click', ()=>{
 company.addEventListener('click', ()=>{
     subMenuCompany.classList.toggle("active");
     arrowCompany.classList.toggle("active");
+})
+
+dtFeatures.addEventListener('click', ()=>{
+    dtSubMenuFeatures.classList.toggle("active");
+    const dtArrowFeatures = document.querySelector(".dt-arrow-one").classList.toggle("active");
+})
+
+dtCompany.addEventListener('click', ()=>{
+    dtSubMenuCompany.classList.toggle("active");
+    const dtArrowCompany = document.querySelector(".dt-arrow-two").classList.toggle("active");
 })
